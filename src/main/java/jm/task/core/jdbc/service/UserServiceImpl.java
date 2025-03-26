50a568889c1c83131ae5f34afcd0a566dc099caa
Artyom Afanasev <afanasev06092000@yandex.ru>
2025-03-26 21:37:34 +0700

    fix: Реализовать один общий объект на класс UserServiceImpl.

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,35 +7,30 @@ import java.util.List;
 
 
 public class UserServiceImpl implements UserService {
+    private UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
         userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
         userDao.saveUser(name, lastName, age);
 
     }
 
     public void removeUserById(long id) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
         userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
         return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
         userDao.cleanUsersTable();
     }
 }
